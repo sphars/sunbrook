d3fdb391bddcd914199925bd862287e3f46e8cbb
Spencer Harston <spencer314@gmail.com>
2020-10-04 23:25:59 -0600

    Add sass preprocessing; add logo to nav

--- .eleventy.js
+++ .eleventy.js
@@ -2,8 +2,8 @@ module.exports = function(eleventyConfig) {
 
     // copy static files to output
     eleventyConfig.addPassthroughCopy({
-        "node_modules/bulma/css/bulma.min.css": "assets/css/bulma.min.css",
-        "node_modules/bulma/css/bulma.css.map": "assets/css/bulma.css.map",
+        // "node_modules/bulma/css/bulma.min.css": "assets/css/bulma.min.css",
+        // "node_modules/bulma/css/bulma.css.map": "assets/css/bulma.css.map",
         "src/assets": "assets/"
     });
 
