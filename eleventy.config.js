e20be39816daaf1eddc9636bd1ced3a3bf5bfec6
Spencer Harston <spencer314@gmail.com>
2024-06-05 22:39:10 -0600

    fix: npm scripts and scss
    - clean up npm scripts, use npm-run-all2
    - use sass instead of node-sass
    - sass add css directly to dist
    - fix hamburger menu

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,10 +2,8 @@ module.exports = function(eleventyConfig) {
 
     // copy static files to output
     eleventyConfig.addPassthroughCopy({
-        // "node_modules/bulma/css/bulma.min.css": "assets/css/bulma.min.css",
-        // "node_modules/bulma/css/bulma.css.map": "assets/css/bulma.css.map",
         "src/assets": "assets/",
-        "src/*.ico":"/",
+        "src/favicon.ico":"/",
         "node_modules/@fontsource/noto-sans/": "assets/fonts/noto-sans/"
     });
 
